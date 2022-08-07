d9ecae5c2598255845600019d1fb893adb819515
RD WebDesign <github@rdwebdesign.com.br>
2022-08-07 17:27:29 -0300

    Using new config
    Signed-off-by: RD WebDesign <github@rdwebdesign.com.br>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,11 +1,29 @@
 <?php
 
-$finder = PhpCsFixer\Finder::create()->in(__DIR__)->exclude('vendor');
+/* Pi-hole: A black hole for Internet advertisements
+*  (c) 2022 Pi-hole, LLC (https://pi-hole.net)
+*  Network-wide ad blocking via your own hardware.
+*
+*  This file is copyright under the latest version of the EUPL.
+*  Please see LICENSE file for your rights under this license.
+*/
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->ignoreDotFiles(false)
+    ->ignoreVCSIgnored(true)
+    ->exclude('scripts/vendor')
+    ->in(__DIR__)
+;
 
 $config = new PhpCsFixer\Config();
-return $config->setRules([
-        '@PSR2' => true,
-        'line_ending' => false,
-    ])
+$config
+    ->setRules(array(
+        '@Symfony' => true,
+        'array_syntax' => array('syntax' => 'long'),
+    ))
     ->setFinder($finder)
 ;
+
+return $config;
